8bffabc8b7377d9a1807412b092b907a90557b83
Leytzher Muro <muro@stim.ai>
2024-07-23 18:09:59 +0400

    AB

--- ab-testing.lisp
+++ ab-testing.lisp
@@ -28,8 +28,32 @@
 
 (defun select-random-ad ()
   (let* ((selection (random (length *ads*)))
-	(reward (display-add (aref *ads* selection))))
+	(reward (display-ad (aref *ads* selection))))
     (values selection reward)))
 
+(defun play-campaign (n-test n-prod)
+  (let* ((n-ads (length *ads*))
+	 (q-values (make-array `(,n-ads) :initial-element 0.0))
+	 (n (make-array `(,n-ads) :initial-element 0))
+	 (total-reward 0)
+	 (average-rewards nil))
+    (dotimes (i n-test)
+      (multiple-value-bind (ad-chosen reward) (select-random-ad)
+	(incf (aref n ad-chosen))
+	(setf (aref q-values ad-chosen)
+	      (+ (aref q-values ad-chosen)
+		 (/ (- reward (aref q-values ad-chosen))
+		    (aref n ad-chosen))))
+	(incf total-reward reward)
+	(push (/ total-reward (1+ i)) average-rewards)))
+    (values (reverse average-rewards)
+	    (position (reduce #'max q-values) q-values)
+	    (reduce #'max q-values))))
+
+
+
+
+    
+	 
 
 
