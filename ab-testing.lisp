6d064341ffa8bc1f105a74401480e07b14d51e57
Leytzher Muro <muro@stim.ai>
2024-07-23 18:22:32 +0400

    AB

--- ab-testing.lisp
+++ ab-testing.lisp
@@ -1,5 +1,6 @@
+(ql:quickload :py4cl)
 (defpackage :ab-testing
-  (:use :cl))
+  (:use :cl :py4cl))
 
 (in-package :ab-testing)
 
@@ -36,6 +37,7 @@
 	 (q-values (make-array `(,n-ads) :initial-element 0.0))
 	 (n (make-array `(,n-ads) :initial-element 0))
 	 (total-reward 0)
+	 (trial-numbers nil)
 	 (average-rewards nil))
     (dotimes (i n-test)
       (multiple-value-bind (ad-chosen reward) (select-random-ad)
@@ -45,15 +47,37 @@
 		 (/ (- reward (aref q-values ad-chosen))
 		    (aref n ad-chosen))))
 	(incf total-reward reward)
+	(push (1+ i) trial-numbers)
 	(push (/ total-reward (1+ i)) average-rewards)))
-    (values (reverse average-rewards)
+    (values
+	    (reverse trial-numbers)
+	    (reverse average-rewards)
 	    (position (reduce #'max q-values) q-values)
 	    (reduce #'max q-values))))
 
 
+(py4cl:python-exec "
+import plotly.graph_objects as go
+import plotly.io as pio
+
+def create_plot(x, y, filename):
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines+markers'))
+    fig.update_layout(title='Average Reward Over Time',
+                      xaxis_title='Number of Trials',
+                      yaxis_title='Average Reward')
+    pio.write_html(fig, file=filename, auto_open=True)
+")
 
 
-    
+(defun run-and-plot-campaign (n-test n-prod filename)
+  (multiple-value-bind (x-data y-data best-ad best-q-value)
+      (play-campaign n-test n-prod)
+    (py4cl:python-call "create_plot" x-data y-data filename)
+    (format t "Best ad: ~A, Q-value: ~A~%" best-ad best-q-value)
+    (format t "Plot saved to ~A~%" filename)))
+
+(run-and-plot-campaign 100000 90000 "campaign_plot.html")
+
 	 
 
 
